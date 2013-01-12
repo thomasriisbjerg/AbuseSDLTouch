599269c06b7b0b2cdbee31c217a6a490ccdcd4cd
Thomas Riisbjerg <thomas.riisbjerg@gmail.com>
2013-01-12 15:23:55 +0100

    Added ascii85 encode/decode support for persisting savegames to scoreloop.

---
+++ src/net/ascii85.h
@@ -0,0 +1,9 @@
+#ifndef __ASCII85_HPP_
+#define __ASCII85_HPP_
+
+void ascii85_encode(const char *infilename, const char *outfilename);
+int ascii85_decode(const char *infilename, const char *outfilename);
+
+#endif
+
+
